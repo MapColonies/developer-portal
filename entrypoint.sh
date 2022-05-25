c79bba1a4711fccaa4db2d5bde82c5d6061f29f0
sapod <sapir.oded1@gmail.com>
2022-05-25 10:07:30 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,12 +17,16 @@ zip -r examples ./*
 
 cd /
 
+echo "Preparing vars"
+
 ENV_TEMPLATE_VALUES=$(printenv | grep 'SERVICE_URL=\|API_KEY=\|EMOJI_CODE=' | cut -d'=' -f1)
 
 for f in $(find /docs/ -type f \( -name "*.js" -o -name "*.html" -o -name "*.md" \)); 
     do
+        echo "Working on file "$f 
         for value in $ENV_TEMPLATE_VALUES;
             do 
+                echo "Applying sed to file"
                 sed -i "s|<$value>|$(printenv $value)|" "$f";
         done
  done
