530928dbb346c12fb688c1996df7eb493abaa4a2
Daniel Hermon <danielhermonsync@gmail.com>
2022-08-01 17:40:04 +0300

    refactor: rename cookbook to developer-portal (#31)

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,33 +4,35 @@ echo cloning from repo $CLASSIFIED_REPO_URL
 
 whoami
 
-REPO_NAME=classified_repo
-cd $REPO_NAME
+CLASSIFIED_REPO_PATH=/app/classified_repo
+cd $CLASSIFIED_REPO_PATH
 
 git clone $CLASSIFIED_REPO_URL .
-\cp -r /$REPO_NAME/docs/. /docs/
-\cp -r /$REPO_NAME/docs/assets/images/. /docs/assets/images
+\cp -r $CLASSIFIED_REPO_PATH/docs/. /app/docs/
+\cp -r $CLASSIFIED_REPO_PATH/docs/assets/images/. /app/docs/assets/images
 
 # Create examples zip
-cd /docs/assets/examples
-zip -r examples ./* 
+cd /app/docs/assets/examples
+zip -r examples ./*
 
 cd /
 
 ENV_TEMPLATE_VALUES=$(printenv | grep 'SERVICE_URL=\|API_KEY=\|EMOJI_CODE=\|INJECTION_TYPE=' | cut -d'=' -f1)
 
-for f in $(find /docs/ -type f \( -name "*.js" -o -name "*.html" -o -name "*.md" \)); 
+for f in $(find /app/docs/ -type f \( -name "*.js" -o -name "*.html" -o -name "*.md" \));
     do
         for value in $ENV_TEMPLATE_VALUES;
-            do 
+            do
                 sed -i "s|<$value>|$(printenv $value)|" "$f";
         done
  done
 
-# rm -r /$REPO_NAME
+# rm -r /$CLASSIFIED_REPO_PATH
 
 # some terminal debug time
 # sleep 10m
+
+cd /app
 
 echo "Running start command"
 exec "$@"
