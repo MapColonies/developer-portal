465079ff0c056daaabbf9df5fec617ea1149886e
shirCitron <shirpb@gamil.com>
2023-11-07 20:51:17 +0200

    docs: add special phrases table with filter

--- docs/MapColonies/openstreetmap/nominatim/faq.md
+++ docs/MapColonies/openstreetmap/nominatim/faq.md
@@ -1,5 +1,5 @@
 ---
-sidebar_position: 5
+sidebar_position: 6
 id: osm-nominatim-faq
 slug: osm-nominatim-faq
 title: FAQ
--- src/components/Vector/SpecialPhrasesTable/index.tsx
+++ src/components/Vector/SpecialPhrasesTable/index.tsx
@@ -1,52 +1,69 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
-import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
-import data from './data.json';
+import { DataGrid, GridColDef, GridColumnMenu, GridColumnMenuProps, GridColumnHeaderParams } from '@mui/x-data-grid';
+import rowsTable from './data.json';
 
+const rows = rowsTable;
 const columns: GridColDef[] = [
   { 
     field: 'phrase', 
-    headerName: 'Phrase', 
     width: 150,
     headerAlign: 'center',
-    align: 'center'
+    align: 'center',
+    renderHeader: () => (<strong>{'Word / Phrase'}</strong>),
   },
   {
     field: 'class',
-    headerName: 'Class',
     width: 150,
     headerAlign: 'center',
-    align: 'center'
+    align: 'center',
+    renderHeader: () => (<strong>{'Class'}</strong>),
   },
   {
     field: 'type',
-    headerName: 'Type',
     width: 150,
     headerAlign: 'center',
-    align: 'center'
+    align: 'center',
+    renderHeader: () => (<strong>{'Type'}</strong>),
   },
   {
     field: 'operator',
-    headerName: 'Operator',
     width: 110,
     headerAlign: 'center',
-    align: 'center'
+    align: 'center',
+    renderHeader: () => (<strong>{'Operator'}</strong>),
   },
   {
     field: 'plural',
-    headerName: 'Plural',
     width: 80,
     headerAlign: 'center',
-    align: 'center'
+    align: 'center',
+    renderHeader: () => (<strong>{'Plural'}</strong>),
   }
 ];
 
-const rows = data;
 
-export default function DataGridDemo() {
+function CustomColumnMenu(props: GridColumnMenuProps) {
   return (
-    <Box sx={{ height: 1000, width: '70%'}}>
+    <GridColumnMenu
+      {...props}
+      slots={{
+        // Hide `columnMenuColumnsItem`
+        columnMenuColumnsItem: null,
+      }}
+    />
+  );
+}
+
+export default function DataGridSpeicalPhrases() {
+  return (
+    <Box sx={{ 
+      height: 1000, 
+      width: '70%'
+    }}>
       <DataGrid
+        slots={{ columnMenu: CustomColumnMenu }} 
+        style={{ backgroundColor: 'white'}}
         rows={rows}
         columns={columns}
         getRowId={row => row.phrase}
