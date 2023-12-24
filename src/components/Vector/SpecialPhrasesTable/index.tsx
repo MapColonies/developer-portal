abf9cdeef69da64a9e06242dfe7ad5789349aaad
Shir Citron <102953166+shirCitron@users.noreply.github.com>
2023-12-24 11:13:48 +0200

    docs: openstreetmap nominatim special phrases (#66)
    * docs: add special phrases to nominatim
    
    * docs: add special phrases table with filter
    
    * style: Special phrases table in dark mode
    
    * fix: fix typo
    
    * fix: fix typo
    
    * docs: add examples for searching with special phrases
    
    * docs: change place of the example
    
    * docs: fix broken link to special phrases
    
    * chore: add a line at the end of the file
    
    * chore: update package lock
    
    ---------
    
    Co-authored-by: shirCitron <shirpb@gamil.com>
    Co-authored-by: shimoncohen <shimoncohen655@gmail.com>

---
+++ src/components/Vector/SpecialPhrasesTable/index.tsx
@@ -0,0 +1,81 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import { DataGrid, GridColDef, GridColumnMenu, GridColumnMenuProps } from '@mui/x-data-grid';
+import rowsTable from './data.json';
+
+
+const rows = rowsTable;
+const columns: GridColDef[] = [
+  { 
+    field: 'phrase', 
+    headerAlign: 'center',
+    align: 'center',
+    flex: 1,
+    renderHeader: () => (<strong>{'Word / Phrase'}</strong>),
+  },
+  {
+    field: 'class',
+    flex: 1,
+    headerAlign: 'center',
+    align: 'center',
+    renderHeader: () => (<strong>{'Key'}</strong>),
+  },
+  {
+    field: 'type',
+    headerName: 'Value',
+    flex: 1,
+    headerAlign: 'center',
+    align: 'center',
+    renderHeader: () => (<strong>{'Value'}</strong>),
+  },
+  {
+    field: 'operator',
+    flex: 1,
+    headerAlign: 'center',
+    align: 'center',
+    renderHeader: () => (<strong>{'Operator'}</strong>),
+  },
+  {
+    field: 'plural',
+    flex: 1,
+    headerAlign: 'center',
+    align: 'center',
+    renderHeader: () => (<strong>{'Plural'}</strong>),
+  }
+];
+
+
+function CustomColumnMenu(props: GridColumnMenuProps) {
+  return (
+    <GridColumnMenu
+      {...props}
+      slots={{
+        // Hide `columnMenuColumnsItem`
+        columnMenuColumnsItem: null,
+      }}
+    />
+  );
+}
+
+
+export default function DataGridSpecialPhrases() {
+  return (
+    <Box sx={{ width: '100%'}}>
+      <DataGrid
+          slots={{ columnMenu: CustomColumnMenu }} 
+          rows={rows}
+          columns={columns}
+          getRowId={row => row.phrase}
+          initialState={{
+            pagination: {
+              paginationModel: {
+                pageSize: 15,
+              },
+            },
+          }}
+          pageSizeOptions={[15, 50, 100]}
+          disableRowSelectionOnClick
+      />
+    </Box>
+  );
+}
