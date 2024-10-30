b6a5c497a9500a6420a4e5a30cf10409268d1322
shimoncohen <shimoncohen@users.noreply.github.com>
2024-10-30 13:19:52 +0000

    Remove preview for PR 115 🛬

--- pr-preview/pr-115/assets/js/bb01961f.3bd05987.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunk_map_colonies_developer_portal=self.webpackChunk_map_colonies_developer_portal||[]).push([[3602],{74298:e=>{e.exports=JSON.parse('{"authors":[{"name":"Daniel Hermon","title":"MapColonies Tech Lead","url":"https://github.com/syncush","imageURL":"https://github.com/syncush.png","key":"hermon","page":null,"count":0}]}')}}]);
