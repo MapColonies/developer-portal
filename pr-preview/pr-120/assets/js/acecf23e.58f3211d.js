23ab773c0f38ab3b5c5e02ca047c3276483ada7b
shimoncohen <shimoncohen@users.noreply.github.com>
2024-11-08 09:38:08 +0000

    Remove preview for PR 120 🛬

--- pr-preview/pr-120/assets/js/acecf23e.58f3211d.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunk_map_colonies_developer_portal=self.webpackChunk_map_colonies_developer_portal||[]).push([[4284],{1912:e=>{e.exports=JSON.parse('{"blogBasePath":"/developer-portal/pr-preview/pr-120/blog","blogTitle":"Release Notes","authorsListPath":"/developer-portal/pr-preview/pr-120/blog/authors"}')}}]);
